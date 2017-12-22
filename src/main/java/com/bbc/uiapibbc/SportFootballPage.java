6074bb9b07104f2d349f10dc7cf5103a8cea6b18
TonyYuta <tonykagaya@gmail.com>
2017-12-21 18:18:53 -0800

    2 pom files

---
+++ src/main/java/com/bbc/uiapibbc/SportFootballPage.java
@@ -0,0 +1,32 @@
+/**
+ *   File Name: SportFootballPage.java<br>
+ *
+ *   Yutaka<br>
+ *   Created: Dec 21, 2017
+ *   
+ */
+
+package com.bbc.uiapibbc;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * SportFootballPage //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      Yutaka
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class SportFootballPage extends BasePage{
+	
+	public SportFootballPage(WebDriver driver) {
+		super(driver);
+	}
+	
+
+}
